ecad4cc6c7a54edb1474ebf19cce80147d62b502
Harry Wilkinson <harrywi@ws45.london.etc>
2025-11-07 18:14:16 +0000

    manual toggle no longer added to undo stack

--- python/houtils/tools/recook.py
+++ python/houtils/tools/recook.py
@@ -5,32 +5,33 @@ parms = ("resimulate", "reload", "clear", "dirtybutton")
 
 
 def main():
-    nodes = hou.sortedNodes(hou.selectedNodes())
-    if not nodes:
-        return
+    with hou.undos.disabler():
+        nodes = hou.sortedNodes(hou.selectedNodes())
+        if not nodes:
+            return
 
-    store_upate_mode = hou.updateModeSetting()
-    hou.setUpdateMode(hou.updateMode.Manual)
-    for node in nodes:
+        store_upate_mode = hou.updateModeSetting()
+        hou.setUpdateMode(hou.updateMode.Manual)
+        for node in nodes:
 
-        # Check if in sim
-        parent = node.parent()
-        while parent is not None:
-            if parent.type().name() == "dopnet":
-                parent.parm("resimulate").pressButton()
-                break
-            parent = parent.parent()
+            # Check if in sim
+            parent = node.parent()
+            while parent is not None:
+                if parent.type().name() == "dopnet":
+                    parent.parm("resimulate").pressButton()
+                    break
+                parent = parent.parent()
 
-        # Reset nodes
-        for parm_name in parms:
-            if parm := node.parm(parm_name):
-                parm.pressButton()
+            # Reset nodes
+            for parm_name in parms:
+                if parm := node.parm(parm_name):
+                    parm.pressButton()
 
-        # Cook Nodes
-        try:
-            node.cook(force=True)
-        except hou.OperationFailed:
-            pass
+            # Cook Nodes
+            try:
+                node.cook(force=True)
+            except hou.OperationFailed:
+                pass
 
-    hou.setUpdateMode(store_upate_mode)
-    background_notify(hou.findFile("config/Icons/recook.png"))
+        hou.setUpdateMode(store_upate_mode)
+        background_notify(hou.findFile("config/Icons/recook.png"))
