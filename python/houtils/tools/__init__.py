29032623d42948fb85326836c5346e57bd0032f6
Harry Wilkinson <harrywi@edge-ws41.london.etc>
2026-02-09 18:02:48 +0000

    edit mode implemented

--- python/houtils/tools/__init__.py
+++ python/houtils/tools/__init__.py
@@ -3,5 +3,6 @@ from .toggle_update import main as toggle_update
 from .reload_modules import main as reload_modules
 from .clear_colors import main as clear_colors
 from .toggle_color_mode import main as toggle_color_mode
+from .set_manual_color import main as set_manual_color
 
-__all__ = ["recook", "toggle_update", "reload_modules", "clear_colors", "toggle_color_mode"]
+__all__ = ["recook", "toggle_update", "reload_modules", "clear_colors", "toggle_color_mode", "set_manual_color"]
